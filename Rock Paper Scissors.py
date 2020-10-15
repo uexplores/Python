63afb0d30df71acbfeb6c9831dbaee433fda473b
uexplores <39589801+uexplores@users.noreply.github.com>
2020-10-15 14:17:45 -0700

    Add files via upload
    I played this game with my son to test the logic ... It was fun!!

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -9,7 +9,13 @@ c = random.choice(b)
 a = str(input("Enter R for Rock, P for Paper, S for Scissors: "))
 print('{} picked {}'.format(name,a))
 print('I picked {}'.format(c))
-if (a == 'R' and c == 'S') or (a == 'S' and c == 'P') or (a == 'P' and c == 'R') or (a == b):
+if (a == b):
+    print("It's a tie! Let's play again ...")
+    a = str(input("Enter R for Rock, P for Paper, S for Scissors: "))
+    c = random.choice(b)
+    print('{} picked {}'.format(name,a))
+    print('I picked {}'.format(c))
+if (a == 'R' and c == 'S') or (a == 'S' and c == 'P') or (a == 'P' and c == 'R'):
     print("{} win".format(name))
 else: 
     print("I win")
